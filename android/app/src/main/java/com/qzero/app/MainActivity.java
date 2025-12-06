fafd5522f3875773269967f0bbb070ee1d41bdcd
Afonso75 <94051437+Afonso75@users.noreply.github.com>
2025-12-06 20:03:58 +0000

    Fix package to com.qzero.app, remove BOM from Java, regen android

---
+++ android/app/src/main/java/com/qzero/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.qzero.app;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // If you have any plugins that require initialization, do it here.
+    }
+}
