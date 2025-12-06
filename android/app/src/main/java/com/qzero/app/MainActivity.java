e14233190ce464ad33ad3a5bce11c863c51c7564
Afonso75 <94051437+Afonso75@users.noreply.github.com>
2025-12-06 20:20:27 +0000

    Fix package com.qzero.app, remove BOMs, regenerate android and build AAB

--- android/app/src/main/java/com/qzero/app/MainActivity.java
+++ android/app/src/main/java/com/qzero/app/MainActivity.java
@@ -1,12 +1,5 @@
 package com.qzero.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // If you have any plugins that require initialization, do it here.
-    }
-}
+public class MainActivity extends BridgeActivity {}
